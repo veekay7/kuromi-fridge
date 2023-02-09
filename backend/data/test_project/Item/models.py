f6a30dc6fc7c3bf3e780ec3825d94216c8bc2509
Hishikawa Junya <h.junya.20000318@gmail.com>
2023-02-09 15:58:58 +0900

    #18 add barcode Field

--- backend/data/test_project/Item/models.py
+++ backend/data/test_project/Item/models.py
@@ -8,6 +8,7 @@ class Item(models.Model):
             (REFRIGERATED, "refrigerated"),
             (FROZEN, "frozen"),
     )
+    barcode = models.PositiveBigIntegerField(null=True)
     name = models.TextField()
     expire_date = models.DateField()
     status = models.CharField(choices=LOCATION_SET, max_length=16, null=True)
